082284fb5ad53dc9c3c7c1a2ee5da65351b6595b
zmain4 <zmain4@proton.me>
2025-03-11 21:02:36 -0500

    replaced prints with logging

---
+++ client/logUtils.py
@@ -0,0 +1,17 @@
+import logging
+import os
+
+# Configure logging
+LOG_FILE = os.path.join(os.getcwd(), "storage", "app.log")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    handlers=[
+        logging.FileHandler(LOG_FILE),  # Log to file
+        logging.StreamHandler()  # Log to console
+    ]
+)
+
+logger = logging.getLogger("AppLogger")
+
